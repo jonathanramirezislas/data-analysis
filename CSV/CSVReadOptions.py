9557677957defaa646138298dba1cea9ebe2df5b
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2020-08-12 11:00:12 -0500

    working model CSV

--- CSV/CSVReadOptions.py
+++ CSV/CSVReadOptions.py
@@ -15,7 +15,9 @@ def dictparse(csvfilename, keyfield, separator, quote, quotestrategy):
         csvreader = csv.DictReader(csvfile,
                                    skipinitialspace=True,
                                    delimiter=separator,
+                                    #ayuda a indicar que signo se va ausar como carater de citas
                                    quotechar=quote,
+                                   #ver en las funciones de abajo su funcionalidad
                                    quoting=quotestrategy)
         for row in csvreader:
             table[row[keyfield]] = row
@@ -46,11 +48,12 @@ print_table(table, fieldnames)
 print("")
 print("")
 
+                                                            # Indica que cualquier cosa que no este entre quoute en ese caso "" va ser un numero y se represatara como tal por ello tenemos valores float ejemplo 65.0
 table2, fieldnames2 = dictparse("hightemp2.csv", 'City', ',', '"', csv.QUOTE_NONNUMERIC)
 print_table(table2, fieldnames2)
 
 print("")
 print("")
 
-table3, fieldnames3 = dictparse("hightemp3.csv", 'City', ',', '"', csv.QUOTE_NONNUMERIC)
+table3, fieldnames3 = dictparse("hightemp3.csv", 'City', ' ', '´', csv.QUOTE_NONNUMERIC)
 print_table(table3, fieldnames3)
